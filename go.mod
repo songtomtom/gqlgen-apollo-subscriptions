83ed81df43374caeadcd6cca6a61dd7c1f850242
songtomtom <tom.song@luxrobo.com>
2023-01-10 17:45:28 +0900

    스키마 정의 후 데이터 베이스 확인

--- go.mod
+++ go.mod
@@ -4,15 +4,17 @@ go 1.18
 
 require (
 	github.com/99designs/gqlgen v0.17.22
+	github.com/gorilla/websocket v1.5.0
 	github.com/vektah/gqlparser/v2 v2.5.1
+	gorm.io/gorm v1.24.3
 )
 
 require (
 	github.com/agnivade/levenshtein v1.1.1 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	gorm.io/gorm v1.24.3 // indirect
+	gorm.io/driver/mysql v1.4.5 // indirect
 )
--- server.go
+++ server.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/gorilla/websocket"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/songtomtom/gqlgen-apollo-subscriptions/graph"
 )
 
 const defaultPort = "8080"
+
+type (
+	Post struct {
+		gorm.Model
+	}
+
+	Comment struct {
+		gorm.Model
+		PostID  int
+		Content string
+	}
+)
 
 func main() {
 	port := os.Getenv("PORT")
@@ -20,26 +34,42 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "test:test@tcp(127.0.0.1:33006)/test?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+
+	if err = db.AutoMigrate(&Post{}, &Comment{}); err != nil {
+		log.Fatalf("failed to auto migration schema: %v", err)
+	}
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-	http.HandleFunc("/subscriptions", func(w http.ResponseWriter, r *http.Request) {
-		_srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
-		_srv.AddTransport(
-			&transport.Websocket{
-				Upgrader: websocket.Upgrader{
-					CheckOrigin: func(r *http.Request) bool {
-						return true
-					},
-					ReadBufferSize:  1024,
-					WriteBufferSize: 1024,
-				},
-			},
-		) // <---- This is the important part!
-		_srv.ServeHTTP(w, r)
-	})
+	http.HandleFunc("/query", queryHandlerFn)
+	http.HandleFunc("/subscriptions", subscriptionHandlerFn)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func queryHandlerFn(w http.ResponseWriter, r *http.Request) {
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv.ServeHTTP(w, r)
+}
+
+func subscriptionHandlerFn(w http.ResponseWriter, r *http.Request) {
+	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv.AddTransport(
+		&transport.Websocket{
+			Upgrader: websocket.Upgrader{
+				CheckOrigin: func(r *http.Request) bool {
+					return true
+				},
+				ReadBufferSize:  1024,
+				WriteBufferSize: 1024,
+			},
+		},
+	) // <---- This is the important part!
+	srv.ServeHTTP(w, r)
 }
